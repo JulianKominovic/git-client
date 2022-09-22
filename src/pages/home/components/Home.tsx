49893359de1a43eb7f62c68c6eb559216093feea
Julian Kominovic <juliankominovic@gmail.com>
2022-09-21 22:32:25 -0300

    #INITIAL

---
+++ src/pages/home/components/Home.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+impo;
+
+type Props = {};
+
+const Home = (props: Props) => {
+  useEffect(() => {}, []);
+
+  return (
+    <div style={{ height: "100vh" }}>
+      <div id="graph"></div>
+    </div>
+  );
+};
+
+export default Home;
